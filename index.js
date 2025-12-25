efea33991b90d995f9cfb79448a4bab1de67546d
Abdullah <work.abdullahamir@gmail.com>
2025-12-25 23:20:00 +0500

    Change middleware trust proxy setting to set method
    Wrong syntax was used

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 
 // Middlewares
-app.use("trust proxy", 1);
+app.set("trust proxy", 1);
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
