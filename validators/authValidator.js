df3804f31eebbec0877aca40c4903f687ab5f753
w-abdullahamir <w-abdullahamir@gmail.com>
2026-02-07 00:53:27 +0500

    Fix syntax errors in auth validation rules

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -2,16 +2,16 @@ const { body } = require("express-validator");
 
 exports.signupValidator = [
 	body("email").isEmail().normalizeEmail().notEmpty(),
-	body("password").isString.isLength({ min: 8 }).notEmpty(),
+	body("password").isString().isLength({ min: 8 }).notEmpty(),
 	body("username").optional().isString().isLength({ min: 5, max: 20 }),
 ];
 
 exports.loginValidator = [
-	body("identifier").isString.trim().notEmpty(),
-	body("password").isString.isLength({ min: 8 }).notEmpty(),
+	body("identifier").isString().trim().notEmpty(),
+	body("password").isString().isLength({ min: 8 }).notEmpty(),
 ];
 
 exports.resetPasswordValidator = [
-	body("token").isNumeric.isLength({ min: 6, max: 6 }).notEmpty(),
-	body("password").isString.isLength({ min: 8 }).notEmpty(),
+	body("token").isNumeric().isLength({ min: 6, max: 6 }).notEmpty(),
+	body("password").isString().isLength({ min: 8 }).notEmpty(),
 ];
